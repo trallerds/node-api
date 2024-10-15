a1ed8521a28a56e3bb48c4c4562d6b4108d2efc9
trallerds <jessicacristec@gmail.com>
2024-10-14 21:20:22 -0300

    feature: add pagination

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const { Sequelize, DataTypes } = require("sequelize");
-const helmet = require('helmet');
-const compression = require('compression');
-const rateLimit = require('express-rate-limit');
+const helmet = require("helmet");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
 require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
-    require: true, 
-    rejectUnauthorize: false
-  }
+    require: true,
+    rejectUnauthorize: false,
+  },
 });
 
 const SensorData = sequelize.define("sensor-data", {
@@ -30,19 +30,30 @@ const SensorData = sequelize.define("sensor-data", {
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 10
+  max: 10,
 });
-
 
 const app = express();
 
 app.use(helmet());
 app.use(compression());
 app.use(express.json());
-app.use(limiter)
+app.use(limiter);
+
+//authorization
+app.use((req, res, next) => {
+  let key = req.query.key;
+  if (!key || key !== "12345") {
+    res.status(403).send();
+    return;
+  }
+  next();
+});
 
 app.get("/data", async (req, res) => {
-  const allData = await SensorData.findAll();
+  let limit = req.query.limit || 5;
+  let offset = req.query.offset || 0;
+  const allData = await SensorData.findAll({ limit, offset });
   res.status(200).send(allData);
 });
 
